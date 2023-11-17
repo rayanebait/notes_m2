9dde9a380167e01d92d5648e1c2360e9e8478916
rayanebait <rayane-bait@outlook.fr>
2023-11-17 16:09:54 +0100

    17/11/2023

--- carte_a_puces/tp/tp4/exercice2.c
+++ carte_a_puces/tp/tp4/exercice2.c
@@ -1,1 +1,25 @@
 #include <avr/io.h>
+#include <util/delay.h>
+
+#include "./uart.h"
+
+#define BAUD_RATE 9600
+#define BLINK_DELAY_MS 2000
+
+void delay500ms(){
+    PORTB ^= _BV(PORTB5);
+	_delay_ms(BLINK_DELAY_MS);
+}
+
+int main(void){
+    // SPCR |= _BV(MSTR) | _BV(SPE);
+    DDRB |= _BV(DDB5);
+
+    UART__init(BAUD_RATE);
+    
+    for(;;){
+        uint8_t c = UART__getc();
+        delay500ms();
+        UART__putc(c);
+    }
+}
