d5612c4fd1490d8f80fa9d5ee9002e793f9c8571
rayanebait <rayane-bait@outlook.fr>
2024-01-26 15:55:35 +0100

    last

--- carte_a_puces/tp/perso/include/utils/adc_setup.h
+++
@@ -1,8 +0,0 @@
-#ifndef ADC_SETUP_H
-#define ADC_SETUP_H
-
-#include <stdint.h>
-
-void Adc_8bit_autotrigger_setup(uint8_t prescaler, char interrupt);
-
-#endif /*ADC_SETUP_H*/
--- carte_a_puces/tp/perso/include/utils/get_approval.h
+++
@@ -1,9 +0,0 @@
-#ifndef GET_APPROVAL_H
-#define GET_APPROVAL_H
-
-#include "error.h"
-
-
-status_t get_approval(void);
-
-#endif
